eadbc2496e72a0e93c9c550607da98d4826cca98
Arti Zirk <arti.zirk@gmail.com>
2016-11-01 23:15:26 +0200

    Add standalone server to rest api server

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,1 +1,8 @@
 from karumedia import application
+
+if __name__ == "__main__":
+    from wsgiref.simple_server import make_server
+
+    httpd = make_server('0.0.0.0', 8080, application)
+    print("Serving on http://0.0.0.0:8080/")
+    httpd.serve_forever()
